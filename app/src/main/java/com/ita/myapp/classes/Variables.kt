e099a39830ad3bd62c761c45cb56941316006b6d
tony <starreach33@gmail.com>
2024-09-02 07:49:47 -0600

    Added Array functions and use of an Object Class

--- app/src/main/java/com/ita/myapp/classes/Variables.kt
+++ app/src/main/java/com/ita/myapp/classes/Variables.kt
@@ -32,7 +32,16 @@ package com.ita.myapp.classes
         println(add())
         println(product(5,2))
 
-    }
+        printArray(names)
+        println(names.joinToString())
+        val numbers = arrayOf(1,2,3,4,5,6,7,8,9)
+        evenOdd(numbers)
+        println(getDay(3))
+
+        val person = Person(19,"Tony");
+
+        person.displayInformation();
+}
 fun add():Int{
     val x = 5
     val y = 10
@@ -42,5 +51,41 @@ fun add():Int{
 
 fun product(x:Int,y:Int):Int{
     return x*y
+}
+
+fun printArray(names:Array<String>){
+    for(name in names){
+        print("Hello $name")
+
+    }
+}
+
+fun evenOdd(numbers:Array<Int>){
+    for(number in numbers){
+        if(number%2 == 0){
+            println("$number is odd")
+        }else{
+            println("$number is even")
+        }
+    }
+}
+fun getDay(day:Int):String{
+    var name = ""
+    when(day){
+        1 ->  name = "Monday"
+        2 -> name = "Tuesday"
+        3 -> name = "Wednesday"
+        4 -> name = "Thursday"
+        5 -> name ="Friday"
+        6 -> name= "Saturday"
+        7 -> name = "Sunday"
+    }
+    return name;
+}
+
+class Person(val age:Int, val name:String){
+    fun displayInformation(){
+        println("Name: $name, Age: $age")
+    }
 }
 //}
